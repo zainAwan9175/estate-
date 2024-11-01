e625a5f46922a066281fffb68ec1e3659299ce69
zain <zain9175zain@gmail.com>
2024-11-01 22:56:44 +0500

    hihi

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ import mongdbconnection from "./mongodb-connection.js";
 mongdbconnection();
 
 app.use(cors({
-    origin: ["https://estate-liard.vercel.app"],
+    origin: ["https://estate-client-iota.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
 }));
